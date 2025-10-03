7b4ec1740ff18b9002b89baf4f9c9512bcae9c56
Kudistalbekov <stalbekovkudaibergen@gmail.com>
2025-10-03 22:26:00 +0600

    learning react

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Profile from './App.tsx'
+import Test from './components/test.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Profile />
+    {/* <Profile /> */}
+    <Test></Test>
   </StrictMode>,
 )
